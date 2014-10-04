57d3751e9f51285971da2ed193d3b7e3f54b9f6f
Dipack P Panjabi <dipack95@gmail.com>
2014-10-04 16:32:10 +0530

    Create ascDescGen.c

---
+++ ascDescGen.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+int main(int argc, char *argv[])
+{
+	int i = 0, len = atoi(argv[1]);
+	FILE *fa, *fd;
+	char fname_asc[128], fname_desc[128];
+	strcpy(fname_asc, "asc_");
+	strcpy(fname_desc, "desc_");
+	strcat(fname_asc, argv[1]);
+	strcat(fname_desc, argv[1]);
+	strcat(fname_asc, ".txt");
+	strcat(fname_desc, ".txt");
+	fa = fopen(fname_asc, "w");
+	fd = fopen(fname_desc, "w");
+	for(i = 0 ; i < len ; i++)
+	{
+		fprintf(fa, "%d\n", i);
+		fprintf(fd, "%d\n", (len - i));
+	}
+	fclose(fa);
+	fclose(fd);
+	return 0;
+}
